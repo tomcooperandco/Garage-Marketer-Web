c9ddb8bc3051f7ff0bc3f08f6cf0f7792a116603
Thomas Cooper <tom@coope.ltd>
2025-03-06 14:15:18 +0000

    Fix for logo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "320263231"
+    serviceWorkerVersion: "3171827882"
   }
 });
